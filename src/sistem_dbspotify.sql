74e190872c5172307e969b2ae5aeca9e7064c712
mfadilfaiz17 <mfadilfaiz17@gmail.com>
2026-06-09 22:52:36 +0700

    fix: Sistem Sql

--- src/sistem_dbspotify.sql
+++ src/sistem_dbspotify.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 25, 2026 at 03:04 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
